7a07236117587b32751ea1d94fa7a695cd81a0ea
Gabriel Santichio <gabriel@santich.io>
2025-04-17 13:55:12 -0300

    feat: configure Nest config module forRoot

--- src/common/config/config.module.ts
+++ src/common/config/config.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule as NestConfigModule } from '@nestjs/config'
 
-@Module({})
+@Module({
+    imports: [
+        NestConfigModule.forRoot({
+            isGlobal: true,
+            cache: true,
+            ignoreEnvFile: false // Change to false to load .env files
+        })
+    ]
+})
 export class ConfigModule {}
