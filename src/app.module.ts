4887b7958b4ab88c680db95a0a3cd4c4edca3a86
Gabriel Santichio <gabriel@santich.io>
2025-04-17 13:24:53 -0300

    feat: create config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from './common/database/database.module'
 import { UserModule } from './modules/user/user.module'
+import { ConfigModule } from './common/config/config.module'
 
 @Module({
-    imports: [DatabaseModule.register(), UserModule],
+    imports: [DatabaseModule.register(), UserModule, ConfigModule],
     controllers: [],
     providers: []
 })
