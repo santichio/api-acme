46de081f1463be517f093bf821cf44468dc91270
Gabriel Santichio <gabriel@santich.io>
2025-04-24 17:09:53 -0300

    cleanup: remove yaml config module

--- src/common/config/schemas/envOptions.schema.ts
+++
@@ -1,8 +0,0 @@
-import * as Joi from 'joi'
-import { envEnum } from '../enums/env.enum'
-
-export const envOptionsSchema = {
-    NODE_ENV: Joi.string()
-        .valid(...Object.values(envEnum))
-        .default(envEnum.DEVELOP)
-}
